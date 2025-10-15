fd4371f92ce01231615a4ff1523d18f6ee155a59
Yuri Dev <147882154+Yurek4@users.noreply.github.com>
2025-10-15 16:38:33 -0300

    Add imports and main loop for resource collection

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pos_inicial
+import Terreno
+import hay
+import Wood
+import carrot
 
+
+pos_inicial.primeira_casa()
+
+
+
+while True:
+
+	
+	if get_ground_type() == Grounds.Grassland:
+		hay.hay_collet()
+		Wood.wood_collet()
+
+	Terreno.prepararTerreno()
+	carrot.carrot_collet()
